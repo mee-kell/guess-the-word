fe87ad1a9c437d14de02641af3fecfa6b4b9e118
Michelle Lo <lomichelle42@gmail.com>
2021-07-02 11:28:55 +0800

    feat: :sparkles: Created an SMS Hangman game!

--- routes/index.js
+++ routes/index.js
@@ -6,31 +6,17 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 // Creates a route at /play-game that Twilio will POST to
 router.post('/play-game', (req, res) => {
-  // creates a new instance of the MessagingResponse object
+  // Creates a new instance of the MessagingResponse object
   const twiml = new MessagingResponse();
 
-  // 💡 How can we clean this data?
-  const incomingMsg = req.body.Body;
-
-  /* BEGIN OF INTRO SECTION
-  👋 Hi! Read me first! 👋 */
-
-  // Prints out the text sent to the Twilio SMS number
+  // Clean data for consistent formatting
+  const incomingMsg = req.body.Body.toLowerCase().trim();
   console.log(incomingMsg);
 
-  // Sends back a message
-  twiml.message("Wow, I sure do love Major League Hacking. Excited for today's INIT challenge!");
-
-  // Using the instructions in the README.md, run the app and send your Twilio SMS number a text
-  // Check out the link on your ngrok tab in terminal, most likely http://localhost:4040
-  // Peruse the debug logs and then when comfortable, delete this section
-
-  /* 👋 See you later, alligator! 🐊
-  (Send in chat "in a while, crocodile" if you're cool)
-  END OF INTRO SECTION */
+  // Twilio Phone Number: +19729145616
 
   // 💡 Add a secret word to test this game with!
-  const word = '';
+  const word = 'tig';
 
   // ✨ Helper functions ✨
     
@@ -39,27 +25,50 @@ router.post('/play-game', (req, res) => {
     req.session.wordState = new Array(word.length).fill('_');
     req.session.lives = 5;
     req.session.playing = true;
-    twiml.message(`Text back one letter at a time to try and figure out the word. If you know the word, text the entire word!\n\nYou have ${req.session.lives} lives left. \n\n ${req.session.wordState.join(' ')}`);
+    twiml.message(`✨ Text back one letter at a time to try and figure out the word. If you know the word, text the entire word! ✨ \n\nYou have ${req.session.lives} lives left. \n\n ${req.session.wordState.join(' ')}`);
   }
 
   const handleInvalidSMS = () => {
     // 💡 Send an error message
+    twiml.message("👋 Welcome to Hangman. Please type 'start' to play.");
   }
 
   const checkForSuccess = () => {
     // 💡 Check to see if player guessed the full word or a letter in it
+    if (incomingMsg == word) { return "win"; }
+    if (word.includes(incomingMsg)) { return "match"; }
+    return false;
   }
 
   const handleGameOver = msg => {
     // 💡 Notify the player that the game is over
+    twiml.message(msg);
+    req.session.destroy();
   }
 
   const handleBadGuess = () => {
     // 💡 Let the player know if their guess was incorrect
+    req.session.lives--;
+    if (req.session.lives == 0) {
+      handleGameOver("You died. 💀 Please start again!")
+    } else {
+      twiml.message(`Oops! You have ${req.session.lives} lives remaining.`)
+    }
   }
 
   const handleMatch = () => {
     // 💡 Update hint with correct guesses
+    for (let [i, char] of [...word].entries()) {
+      if (char == incomingMsg) {
+        req.session.wordState[i] = incomingMsg;
+      }
+    }
+
+    if (req.session.wordState.join('') == word) {
+      handleGameOver(`The word was ${word}! You won 🎉`)
+    } else {
+      twiml.message(`You got a letter! 👍 \n\n${req.session.wordState.join(' ')}`)
+    }
   }
 
   // 🎮 Game Play Logic 🎮
@@ -67,15 +76,23 @@ router.post('/play-game', (req, res) => {
   if (!req.session.playing) {
     // 💡 Set up game logic with the helper functions
     if (incomingMsg == 'start') {
-      // ❓ If you're not playing someone texts you start, what helper function do you call?
+      handleNewGame();
     } else {
-
+      handleInvalidSMS();
     }
   } else {
     // 💡 Logic once you've started playing the game!
+    const success = checkForSuccess();
+    if (success == "win") {
+      handleGameOver(`The word was ${word}! You won 🎉`);
+    } else if (success == "match") {
+      handleMatch();
+    } else {
+      handleBadGuess();
+    }
   }
 
-  // sends the response back to the user
+  // Sends the response back to the user
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
